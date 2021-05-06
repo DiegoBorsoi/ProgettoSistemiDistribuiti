6d604589ed96664d1cbd0786303e846e3992c269
Filippo Callegari <callegari.filippo@gmail.com>
2021-05-06 17:38:40 +0200

    handle shutdown node
    correct shutdown handling  from ambiente wt graph consistency

--- Progetto/src/ambiente.erl
+++ Progetto/src/ambiente.erl
@@ -65,44 +65,38 @@ handle_call(_Request, _From, State = #ambiente_state{}) ->
   {reply, ok, State}.
 
 handle_cast({ignore_nb, Id_ignoring, Id_ignored}, State = #ambiente_state{graph = Graph, comm_spwn = Comm}) ->
-  [[Neighbs]] = ets:match(Graph, {Id_ignoring, '_', '$1'}),
-  case [Id_ignored] -- Neighbs of
-    [] ->
-      try
-        comm_ambiente:ignore_neighb(maps:get(Id_ignoring, Comm), Id_ignored),
-        % aggiorniamo la tabella contenente la struttura del grafo
-        [{_, Tipo_ignoring, NBL_ignoring}] = ets:lookup(Graph, Id_ignoring),
-        ets:insert(Graph, {Id_ignoring, Tipo_ignoring, NBL_ignoring -- [Id_ignored]}),
-        [{_, Tipo_ignored, NBL_ignored}] = ets:lookup(Graph, Id_ignored),
-        ets:insert(Graph, {Id_ignored, Tipo_ignored, NBL_ignored -- [Id_ignoring]})
-      catch
-        _:_ ->
-          io:format("ambiente: Errore -> Nodo ignorante errato (~p, ~p).~n", [Id_ignoring, Id_ignored])
-      end;
-    _ ->
-      io:format("ambiente: Errore -> Nodo vicino errato (~p, ~p).~n", [Id_ignoring, Id_ignored])
+  try
+    [[Neighbs]] = ets:match(Graph, {Id_ignoring, '_', '$1'}),
+    case [Id_ignored] -- Neighbs of
+      [] ->
+        try
+          comm_ambiente:ignore_neighb(maps:get(Id_ignoring, Comm), Id_ignored),
+          % aggiorniamo la tabella contenente la struttura del grafo
+          [{_, Tipo_ignoring, NBL_ignoring}] = ets:lookup(Graph, Id_ignoring),
+          ets:insert(Graph, {Id_ignoring, Tipo_ignoring, NBL_ignoring -- [Id_ignored]}),
+          [{_, Tipo_ignored, NBL_ignored}] = ets:lookup(Graph, Id_ignored),
+          ets:insert(Graph, {Id_ignored, Tipo_ignored, NBL_ignored -- [Id_ignoring]})
+        catch
+          _:_ ->
+            io:format("ambiente: Errore -> Nodo ignorante errato (~p, ~p).~n", [Id_ignoring, Id_ignored])
+        end;
+      _ ->
+        io:format("ambiente: Errore -> Nodo vicino errato (~p, ~p).~n", [Id_ignoring, Id_ignored])
+    end
+  catch
+    _ : _ ->
+      io:format("ambiente: impossibile far ignorare i nodi (~p, ~p).~n", [Id_ignoring, Id_ignored])
   end,
   {noreply, State};
-handle_cast({kill_node, Id},
-    State = #ambiente_state{
-      graph = Graph,
-      id_sup_node = MsupNode,
-      id_spwn = Mspw,
-      comm_spwn = Mcommspw}) ->
+handle_cast({kill_node, Id}, State = #ambiente_state{id_sup_node = MsupNode}) ->
   try
-    {Pid_sup_node, _} = maps:take(Id, MsupNode),
-    ets:delete(Graph, Id),  %TODO: rimuoverlo dai vicini
+    Pid_sup_node = maps:get(Id, MsupNode),
     exit(Pid_sup_node, shutdown)
   catch
     _:_ ->
       io:format("ambiente: unable to locate child(~p).\n", [Id])
   end,
-  {noreply,
-    State#ambiente_state{
-      id_sup_node = maps:remove(Id, MsupNode),
-      id_spwn = maps:remove(Id, Mspw),
-      comm_spwn = maps:remove(Id, Mcommspw)
-    }};
+  {noreply, State};
 handle_cast(_Request, State = #ambiente_state{}) ->
   {noreply, State}.
 
@@ -128,6 +122,41 @@ handle_info({nodo_avviato, Name, {Id, HB_name}}, State = #ambiente_state{graph =
     NeightboardsList
   ),
   {noreply, State#ambiente_state{id_spwn = maps:put(Id, HB_name, ID_Spwn), comm_spwn = maps:put(Id, Name, Comm)}};
+handle_info({'EXIT', Pid, shutdown},
+    State = #ambiente_state{
+      graph = Graph,
+      id_sup_node = MsupNode,
+      id_spwn = Mspw,
+      comm_spwn = Mcommspw}) ->
+  Lillo = try
+            [{Id, _}] = maps:to_list(
+              maps:filter(fun(_Key, Val) ->
+                Val == Pid
+                          end,
+                MsupNode)),
+            ets:take(Graph, Id),
+            ets:foldl(
+              fun({Idg, Tpeg, Nb}, _) ->
+                case ([Id] -- Nb) of
+                  [] -> ets:insert(Graph, {Idg, Tpeg, Nb -- [Id]});
+                  _ -> ok
+                end
+              end,
+              [],
+              Graph
+            ),
+            io:format("ambiente: nodo(~p) correttamente spento.~n", [Id]),
+            Id
+          catch
+            _:_ ->
+              io:format("ambiente: PID(~p) non di mia competenza!~n", [Pid]),
+              undefined
+          end,
+  {noreply, State#ambiente_state{
+    id_sup_node = maps:remove(Lillo, MsupNode),
+    id_spwn = maps:remove(Lillo, Mspw),
+    comm_spwn = maps:remove(Lillo, Mcommspw)
+  }};
 handle_info(Msg, State = #ambiente_state{}) ->
   io:format("ambiente: messaggio sconosciuto -> ~p.~n", [Msg]),
   {noreply, State}.
